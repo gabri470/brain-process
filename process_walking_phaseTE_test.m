86cf325f16eea8d2663db811be28b9ed013a3923
Gabriele Arnulfo <gabriele.arnulfo@gmail.com>
2017-03-01 18:22:27 +0100

    Coherence - bugfixing and including preliminar code for plv/amp

--- process_walking_phaseTE_test.m
+++ process_walking_phaseTE_test.m
@@ -26,7 +26,7 @@ end
 %% ===== GET DESCRIPTION =====
 function sProcess = GetDescription() %#ok<DEFNU>
     % Description the process
-    sProcess.Comment     = 'phasTE';
+    sProcess.Comment     = 'phaseTE';
     sProcess.FileTag     = '__';
     sProcess.Category    = 'Custom';
     sProcess.SubGroup    = 'Walking';
@@ -69,20 +69,23 @@ function OutputFiles = Run(sProcess, sInputs) %#ok<DEFNU>
 	walkingConMask 		= ~cellfun(@isempty,regexp(conditionStrings,'(w|W)alking'));
 	restingConMask 		= ~cellfun(@isempty,regexp(conditionStrings,'(r|R)esting'));
 
-	f1 = figure('papertype','a4','paperposition',[0 0 1 1],...
-                    'paperunits','normalized','paperorientation',...
-						'portrait','visible','on');
+	f1 = figure('papertype','a4','paperorientation',...
+									'portrait','visible','on');
                                     
-	f2 = figure('papertype','a4','paperposition',[0 0 1 1],...
-                    'paperunits','normalized','paperorientation',...
-						'portrait','Visible','on');
+	f2 = figure('papertype','a4','paperorientation',...
+									'portrait','Visible','on');
                                     
-    f3 = figure('papertype','a4','paperposition',[0 0 1 1],...
-                   'paperunits','normalized','paperorientation',...
-                        'portrait','Visible','on');
-
+  f3 = figure('papertype','a4','paperorientation',...
+                  'portrait','Visible','on');
+                                    
+  f4 = figure('papertype','a4','paperorientation',...
+                  'portrait','Visible','on');
 
 	OutputFiles = {};
+
+	nPermutation = 10;
+	nFilters		 = 12;
+	alpha				 = 0.05;
 
 	for subjIdx = 1:nSubjects
 			% for each subject separately we pick standing condition
@@ -103,14 +106,16 @@ function OutputFiles = Run(sProcess, sInputs) %#ok<DEFNU>
 			standingStruct  = preproc(standingStruct,standiChannels,standChFlag,3);
 			% we should here quantify the STN coherence
 			standCoh		= computeCoherence(standingStruct,{standChannels.Channel(standiChannels).Name});
-            [standPvalue,~, standAvgSurr] = runPermutationTest(standCoh,standingStruct,10,0.05);
+      [~,standPvalue,standAvgSurr,standStdSurr] = runPermutationTest(standCoh,standingStruct,nPermutation,alpha);
 
-			restingStruct   = struct('dimord',[],'trial',[],'time',[],'label',[],'elec',[]);
+%			filteredstandingStruct = narrowBandFiltering(standingStruct, nFilters,standiChannels,standChFlag,3);
+%			[standPlv, f] = computePhaseMetric(filteredstandingStruct,standingStruct.label,nPermutation);
+			restingStruct = struct('dimord',[],'trial',[],'time',[],'label',[],'elec',[]);
 
 			% resting recordings can be more than 1
 			for restIdx = 1:numel(restingFileIdx)
 					% read resting time-freq data
-					restChFlag 			= getfield(...
+					restChFlag 	= getfield(...
 											in_bst_data(sInputs(restingFileIdx(restIdx)).FileName,...
 											'ChannelFlag'),'ChannelFlag');
 
@@ -136,8 +141,10 @@ function OutputFiles = Run(sProcess, sInputs) %#ok<DEFNU>
 			restingStruct = rmfield(restingStruct,'sampleinfo');
 			restingStruct.hdr.nTrials = numel(restingStruct.trial);
 			restCoh	= computeCoherence(restingStruct,restingStruct.label);
+%			filteredWalkingStruct = narrowBandFiltering(walkingStruct, nFilters);
+%			[walkPlv, f] = computePhaseMetric(filteredWalkingStruct,walkingStruct.label);
             
-            [restPvalue,~, restAvgSurr] = runPermutationTest(restCoh,restingStruct,10,0.05);
+      [~,restPvalue, restAvgSurr,restStdSurr] = runPermutationTest(restCoh,restingStruct,nPermutation,alpha);
 
 			walkingStruct   = struct('dimord',[],'trial',[],'time',[],'label',[],'elec',[]);
 
@@ -151,6 +158,7 @@ function OutputFiles = Run(sProcess, sInputs) %#ok<DEFNU>
 					walkiChannels	= channel_find(walkChannels.Channel,'SEEG');
 				
 					walkIdxStruct = preproc(walkIdxStruct,walkiChannels,walkChFlag,3);
+%					filteredWalkingStruct = narrowBandFiltering(walkingStruct, nFilters,walkiChannels,walkChFlag,3);
 					if isempty(walkIdxStruct)
 							continue
 					end
@@ -164,56 +172,91 @@ function OutputFiles = Run(sProcess, sInputs) %#ok<DEFNU>
 		
 		  end % walking trial loop
 
-          walkingStruct = rmfield(walkingStruct,'sampleinfo');
-          walkingStruct.hdr.nTrials = numel(walkingStruct.trial);
-          walkCoh	= computeCoherence(walkingStruct,walkingStruct.label);
-          [walkPvalue,~, walkAvgSurr] = runPermutationTest(walkCoh,walkingStruct,10,0.05);
+			walkingStruct = rmfield(walkingStruct,'sampleinfo');
+			walkingStruct.hdr.nTrials = numel(walkingStruct.trial);
+			walkCoh	= computeCoherence(walkingStruct,walkingStruct.label);
+%			[walkPlv, f] = computePhaseMetric(filteredWalkingStruct,walkingStruct.label);
+			[~,walkPvalue, walkAvgSurr,walkStdSurr] = runPermutationTest(walkCoh,walkingStruct,nPermutation,alpha);
+
 
 		  ax1 = subplot(2,4,subjIdx,'NextPlot','add');
-		  plot(restCoh.freq,abs(restCoh.cohspctrm),'LineWidth',2);
-          plot(restCoh.freq,abs(restAvgSurr),'--');
-	%	  plot(standCoh.freq,abs(standCoh.cohspctrm),'LineWidth',2);
-	%	  plot(walkCoh.freq,abs(walkCoh.cohspctrm),'LineWidth',2);	
-     %     legend({'rest','stand','walk'});		
+		  plot(restCoh.freq,abs(restCoh.cohspctrm),'LineWidth',1);
+		  plot(standCoh.freq,abs(standCoh.cohspctrm),'LineWidth',1);
+      plot(walkCoh.freq,abs(walkCoh.cohspctrm),'LineWidth',1);
+		  legend({'rest','stand','walk'});
 		  xlim([6 60]);
+			ylim([0 1]);
 		  title(subjectNames(subjIdx));
-		  set(ax1,'Parent',f1);
+			xlabel('Hz');
+			ylabel('Coh');
+		  set(ax1,'Parent',f1);	
 
 		  ax2 = subplot(2,4,subjIdx,'NextPlot','add');
-		%  plot(restCoh.freq,abs(imag(restCoh.cohspctrm)),'LineWidth',2);
-		  plot(standCoh.freq,abs(imag(standCoh.cohspctrm)),'LineWidth',2);
-          plot(standCoh.freq,abs(standAvgSurr),'--');
-		%  plot(walkCoh.freq,abs(imag(walkCoh.cohspctrm)),'LineWidth',2);	
-		%  legend({'rest','stand','walk'});
+		  plot(standCoh.freq,abs(standCoh.cohspctrm),'LineWidth',1);
+			standCoh.cohspctrm( standPvalue >= 0.05 ) = NaN;
+		  plot(standCoh.freq,abs(standCoh.cohspctrm),'LineWidth',2);
+      plot(standCoh.freq,abs(standAvgSurr),'--');
+%		  plot(restCoh.freq,abs(imag(restCoh.cohspctrm)),'LineWidth',2);
+%		  plot(walkCoh.freq,abs(imag(walkCoh.cohspctrm)),'LineWidth',2);	
+%		  legend({'rest','stand','walk'});
 		  xlim([6 60]);
+			ylim([0 1]);
 		  title(subjectNames(subjIdx));
+			xlabel('Hz');
+			ylabel('Coh');
 		  set(ax2,'Parent',f2);
           
-          
 		  ax3 = subplot(2,4,subjIdx,'NextPlot','add');
-		%  plot(restCoh.freq,abs(imag(restCoh.cohspctrm)),'LineWidth',2);
-		%  plot(standCoh.freq,abs(imag(standCoh.cohspctrm)),'LineWidth',2);
-		  plot(walkCoh.freq,abs((walkCoh.cohspctrm)),'LineWidth',2);
-          plot(walkCoh.freq,abs(walkAvgSurr),'--');
-		%  legend({'rest','stand','walk'});
+%		  plot(restCoh.freq,abs(imag(restCoh.cohspctrm)),'LineWidth',2);
+%		  plot(standCoh.freq,abs(imag(standCoh.cohspctrm)),'LineWidth',2);
+		  plot(walkCoh.freq,abs(walkCoh.cohspctrm),'LineWidth',1);
+			walkCoh.cohspctrm( walkPvalue >= 0.05) = NaN;
+		  plot(walkCoh.freq,abs(walkCoh.cohspctrm),'LineWidth',2);
+      plot(walkCoh.freq,abs(walkAvgSurr),'--');
+%		  legend({'rest','stand','walk'});
 		  xlim([6 60]);
+			ylim([0 1]);
 		  title(subjectNames(subjIdx));
+			xlabel('Hz');
+			ylabel('Coh');
 		  set(ax3,'Parent',f3);
 
+		  ax4 = subplot(2,4,subjIdx,'NextPlot','add');
+		  plot(restCoh.freq,abs(restCoh.cohspctrm),'LineWidth',1);
+			restCoh.cohspctrm( restPvalue >= 0.05 ) = NaN;
+		  plot(restCoh.freq,abs(restCoh.cohspctrm),'LineWidth',2);
+      plot(restCoh.freq,abs(restAvgSurr),'--');
+%		  plot(standCoh.freq,abs(standCoh.cohspctrm),'LineWidth',2);
+%		  plot(walkCoh.freq,abs(walkCoh.cohspctrm),'LineWidth',2);	
+%     legend({'rest','stand','walk'});		
+		  xlim([6 60]);
+			ylim([0 1]);
+		  title(subjectNames(subjIdx));
+			xlabel('Hz');
+			ylabel('Coh');
+		  set(ax4,'Parent',f4);
+
 	end % subject loop
+%
+	print(f1,'/home/gabri/Dropbox/Isaias_group/walking/all.ps','-dpsc2');
+	print(f2,'/home/gabri/Dropbox/Isaias_group/walking/standing.ps','-dpsc2 ');
+	print(f3,'/home/gabri/Dropbox/Isaias_group/walking/walking.ps','-dpsc2 ');
+	print(f4,'/home/gabri/Dropbox/Isaias_group/walking/resting.ps','-dpsc2 ');
+
 	clearvars walkData standData
 end % function
 
 
-function [pvalue, unCorrpvalue, avgSurrogate] = runPermutationTest(dataObs, data, nPermutation,alpha)
+function [pvalue, unCorrpvalue, avgSurrogate,stdSurrogate] = runPermutationTest(dataObs, data, nPermutation,alpha)
 %RUNPERMUTATIONTEST Description
 %	PVALUE = RUNPERMUTATIONTEST(STANCE,SWING,NPERMUTATION) Long description
 %
 
-		dataObs 	 = dataObs.cohspctrm;
+		dataObs 	 = abs(dataObs.cohspctrm);
 		
 		pvalue  	 = zeros(size(dataObs));
-        avgSurrogate = zeros(size(dataObs));
+    avgSurrogate = zeros(size(dataObs));
+    avgSurrSquare = zeros(size(dataObs));
 
 		% we perform a permutation test
 		for permIdx = 1:nPermutation
@@ -221,26 +264,32 @@ function [pvalue, unCorrpvalue, avgSurrogate] = runPermutationTest(dataObs, data
 
 				dat = data.trial{trialIdx};
 				splitOffset = randi(size(dat,2),1);
+				% probably this can be done also using circshift?
 				dat(2,:) = [dat(2,splitOffset:end) dat(2,1:splitOffset-1)]; 
 				data.trial{trialIdx} = dat;
 
 			end
 
 			% compute permutated statistics
-			dataPerm = computeCoherence(data,data.label);
-			dataPerm = dataPerm.cohspctrm;
-            avgSurrogate = avgSurrogate + dataPerm;
+			dataPerm 			= computeCoherence(data,data.label);
+			dataPerm 			= abs(dataPerm.cohspctrm);
+			% x
+      avgSurrogate 	= avgSurrogate + dataPerm;
+			% x^2
+			avgSurrSquare = avgSurrSquare + dataPerm.^2;
 		
 			% compute pvalues for all frequencies and all time points.
-			pvalue = pvalue + double((abs(dataPerm)' > abs(dataObs)))./nPermutation;
+			pvalue = pvalue + double((abs(dataPerm) >= abs(dataObs)))./nPermutation;
 			
-        end
+    end
 
-        avgSurrogate = avgSurrogate ./ nPermutation;
+    avgSurrogate = avgSurrogate ./ nPermutation;
+		stdSurrogate = sqrt((avgSurrSquare./nPermutation) - avgSurrogate);
 		unCorrpvalue = pvalue;
-		pvalue = fdrCorrection(pvalue,alpha);
+		pvalue 			 = fdrCorrection(pvalue,alpha);
 
 end
+
 
 
 function pvalue = fdrCorrection(pvalue, alpha)
@@ -252,12 +301,16 @@ function pvalue = fdrCorrection(pvalue, alpha)
 	N 					= numel(pvalue);
 	FDR 				= alpha.*(1:N)./N;
 	thr 				= FDR(find(tmpPvalue <= FDR',1,'last'));
-	pvalue(pvalue >= thr) = 1;
+
+	if isempty(thr)
+			pvalue  = ones(size(thr));
+	else
+		pvalue(pvalue >= thr) = 1;
+	end
 
 end
 
 function coh = computeCoherence(data,channelNames)
-
 
 	cfg = [];
 
@@ -275,7 +328,7 @@ function coh = computeCoherence(data,channelNames)
 	tapNW						= 2;
 	cfg.keeptrials 	= 'yes';
 	cfg.method  		= 'mtmfft';
-	cfg.foi 				= 1:3:60;
+	cfg.foi 				= 1:.5:60;
 	cfg.pad 				= 'nextpow2';
 	cfg.tapsmofrq 	= tapNW*fs/length(data.time{1});
 
@@ -288,8 +341,76 @@ function coh = computeCoherence(data,channelNames)
 
 end
 
+function [pTE, plv, f, nPLV] = computePhaseMetric(data,channelNames,nPermutation)
+% Description
+%	[PTE,PLV, F] = computePhaseMetric(data,channelNames)
+%
+	chMask	 = ~cellfun(@isempty,regexp(channelNames,'E[0-9]*'));
+	nFilters = numel(data);
+	nTrials  = numel(data(1).trial);
+	plv 		 = complex(zeros(nFilters,nTrials),zeros(nFilters,nTrials));
+	pTE 		 = zeros(nFilters,nTrials);
+	plvSurr	 = complex(zeros(nFilters,nTrials),zeros(nFilters,nTrials));
 
-function pTE=phaseTE(Xf,lag)
+	for fIdx = 1:nFilters
+
+	  % check if any trial contains NaN values and discard it
+		trlMask  = cellfun(@(x) sum(isnan(x),2),data(fIdx).trial,'uni',false);
+		trlMask  = reshape(cat(1,trlMask{:}),2,numel(data(fIdx).trial));
+		trlMask	 = sum(trlMask,1) == 0;
+		nTrials  = numel(trlMask);
+		trialIndices = 1:nTrials;
+
+		for trialIdx = trialIndices(trlMask)
+%			function [pTE, plv, plvSurr] = phaseTE(Xf,lag,nPermutation)
+			[plv(fIdx,trialIdx),plvSurr(fIdx,trialIdx,:)] = ...
+					phaseAmplitudeCorrelation(data(fIdx).trial{trialIdx}(chMask,:),nPermutation);
+
+		end
+		
+	end
+	% get the mean across trials for each filter
+	plv 		= squeeze(mean(plv,2));
+
+	% get the mean across trials and across permutations for each filter
+	plvSurr = squeeze(mean(mean(plvSurr,2),3));
+
+	% this represent p>0.05 confidence limit
+	nPLV 		= plv./plvSurr;
+
+end
+
+function [plv,plvSurr,ampCorr,ampCorrSurr] = phaseAmplitudeCorrelation(X,nPermutation)
+% Description
+%	[PLV,PLVSURR,AMPCORR,AMPCORRSURR] = phaseAmplitudeCorrelation(X,nPermutation) 
+%
+%		%% Inst. phase
+		Xfc= hilbert(X')';
+		Xfh=angle(Xfc);
+		amp=abs(Xfc);
+
+		ampCorr = corrcoef(amp');
+		ampCorr = ampCorr(1,2);
+
+		phi=diff(Xfh);
+		plv=mean(exp(1i*phi),2);
+
+
+		for pIdx = 1:nPermutation
+				% compute surrogate
+				offset 						= randi(size(Xfc,2),1);
+				Xfc(2,:)					= circshift(Xfc(2,:),offset);
+
+				phi								= diff(angle(Xfc));
+				plvSurr(pIdx)			= mean(exp(1i*phi),2);
+				tmp								= corrcoef(abs(Xfc)');
+				ampCorrSurr(pIdx) = tmp(1,2);
+		end
+end
+
+
+
+function [pTE ] = phaseTE(Xf,lag,nPermutation)
 % Inputs:
 % Xf is 2D matrix, i.e. channels x samples
 % lag is the expected delay, expressed in samples
@@ -299,9 +420,9 @@ function pTE=phaseTE(Xf,lag)
 %
 % Function written by Nitin Williams, with help from Felix Siebenhuehner and Muriel Lobier 
 %
-		data 						= ft_preprocessing(cfg,data);
 		%% Inst. phase
-		Xfh=angle(hilbert(Xf'))';
+		Xfh=angle(hilbert(Xf')');
+
 
 		%% Number of bins
 
@@ -336,7 +457,6 @@ function pTE=phaseTE(Xf,lag)
 		[P_XXdYd,~,~]=crosstab(tXh,tXdh,tYdh); P_XXdYd=P_XXdYd./sum(P_XXdYd(:));
 
 		%% Entropy & pTE calculation
-
 		H_YYd = sum(-(P_YYd(P_YYd>0).*(log2(P_YYd(P_YYd>0)))));
 		H_YdXd = sum(-(P_YdXd(P_YdXd>0).*(log2(P_YdXd(P_YdXd>0)))));
 		H_Yd = sum(-(P_Yd(P_Yd>0).*(log2(P_Yd(P_Yd>0)))));
@@ -378,11 +498,12 @@ function data = preproc(data,iChannels,chFlag,trialLength)
 	nTrials   			= floor(totLength/trialLength);
 	offset 					= totLength/2;
 	analysisWindow	= nTrials*trialLength;
-	startTime 			= offset-analysisWindow/2;
-	endTime   			= analysisWindow/2+offset;
+	startTime 			= offset - analysisWindow/2;
+	endTime   			= analysisWindow/2+offset;;
+
 
 	trials					= cat(2,(startTime:trialLength:endTime-trialLength)',...
-													(startTime+trialLength:trialLength:endTime)',zeros(nTrials,1));
+										 (startTime+trialLength:trialLength:endTime)',zeros(nTrials,1));
 
 	trials					= floor(trials*fs)+1;
 	cfg.trl					= trials;
@@ -392,7 +513,48 @@ function data = preproc(data,iChannels,chFlag,trialLength)
 	cfg 						= [];
 	cfg.continuous  = 'yes';
 	cfg.channel			=	data.label(iChannels); 
+	cfg.detrend			= 'yes';
 	data 						= ft_preprocessing(cfg,data);
 
 end
 
+
+function [dataFiltered, f] = narrowBandFiltering(data,nFilters,iChannels,chFlag,trialLength)
+% Description
+%	DATAFILTERED = () Long description
+%
+
+	% nominal frequency and central frequency
+	fn = 2;
+	% band-flat top and band pass width
+	wb = 0.25;
+	% stop band
+	ws = 2;
+	% multiplier 
+	m = sqrt(2);
+
+	f = zeros(nFilters,1);
+	for fIdx = 1:nFilters
+
+			passBandLp = fn + (wb * fn)/2;
+			passBandHp = fn - (wb * fn)/2;
+			stopBandLp = fn * ws;
+			stopBandHp = fn / ws;
+			f(fIdx) = fn;
+			fn = fn * m;
+
+			cfg.lpfreq = passBandLp;
+			cfg.hpfreq = passBandHp;
+			cfg.lpfilter = 'yes';
+			cfg.hpfilter = 'yes';
+			cfg.lpfiltertype = 'but';
+			cfg.hpfiltertype = 'but';
+			cfg.lpfiltdir = 'twopass';
+			cfg.hpfiltdir = 'twopass';
+			data = ft_preprocessing(cfg,data);
+			dataFiltered(fIdx) = preproc(data,iChannels,chFlag,trialLength);
+
+	end
+
+
+end
